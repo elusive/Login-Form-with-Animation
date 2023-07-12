9c94d173a218937587868f81efc238d0a6968754
elusive <2197797-elusive@users.noreply.replit.com>
2023-07-12 16:32:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const signupButton = document.getElementById('signUp');
+const signinButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signupButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signinButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
